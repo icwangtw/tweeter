4ad766e1dc385c96cab94288011ce164fe51cc61
Stephen <stephenicwang@gmail.com>
2018-04-26 23:29:12 +0000

    switched database from memory to MongoDB

---
+++ mongo_example.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const MongoClient = require("mongodb").MongoClient;
+const MONGODB_URI = "mongodb://localhost:27017/tweeter";
+
+MongoClient.connect(MONGODB_URI, (err, db) => {
+  if (err) {
+    console.error(`Failed to connect: ${MONGODB_URI}`);
+    throw err;
+  }
+
+  // We have a connection to the "tweeter" db, starting here.
+  console.log(`Connected to mongodb: ${MONGODB_URI}`);
+
+  // ==> Refactored and wrapped as new, tweet-specific function:
+
+function getTweets(callBackFunc) {
+  db.collection("tweets").find().toArray(callBackFunc);
+}
+
+  // ==> Later it can be invoked. Remember even if you pass
+  //     `getTweets` to another scope, it still has closure over
+  //     `db`, so it will still work. Yay!
+
+  getTweets(function(err, tweets) {
+    console.log("Logging each tweet:");
+    for (let tweet of tweets) {
+      console.log(tweet);
+    }
+    db.close();
+  });
+
+});
