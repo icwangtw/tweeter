711bab8e187fe6898b51db911db5938bd7cb5c2b
Stephen <stephenicwang@gmail.com>
2018-04-26 02:01:06 +0000

    now XSS safe

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,53 +5,36 @@
  */
 
 $(document).ready(function() {
-
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": {
-          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-        },
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": {
-          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-        },
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    },
-    {
-      "user": {
-        "name": "Johann von Goethe",
-        "avatars": {
-          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-        },
-        "handle": "@johann49"
-      },
-      "content": {
-        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-      },
-      "created_at": 1461113796368
+  let $submission = $('#tweet-submit')
+    .on('submit', function(event) {
+    event.preventDefault()
+    if ($submission.serialize().length < 6) {
+      alert("No input message!");
+      return;
+    } else if ($submission.serialize().length > 145) {
+      alert("Message length exceeded 140 characters!");
+      return;
     }
-  ];
+    $.ajax({
+      url: '/tweets',
+      method: 'POST',
+      data: $submission.serialize(),
+      success: function() {
+        loadTweets()
+        $submission[0].reset()
+      }
+    });
+  });
+
+  function loadTweets () {
+    $.ajax({
+      url: '/tweets',
+      method: 'GET',
+      success: function(data) {
+        renderTweets(data.reverse());
+      }
+    });
+  }
 
   function calcTime(inputTime) {
     let datenow = Date.now() - inputTime;
@@ -67,24 +50,23 @@ $(document).ready(function() {
   function createTweetElement(tweetData) {
     const $tweetHTML = $("<article></article");
     const $header = $("<header></header>");
-    const $tweetText = $("<p class ='tweet-text'>" + tweetData.content.text + "</p>");
+    const $tweetText = $("<p>").text(tweetData.content.text).addClass("tweet-text");
     const icons = "<i class='fas fa-flag'></i><i class='fas fa-retweet'></i><i class='fas fa-heart'></i>";
     const tweetTime = calcTime(tweetData.created_at);
     const $footer = $("<footer><span class ='time'>" + tweetTime + "</span><span class = 'tweeter-icons'>"+ icons +"</span> </footer>");
     $($header).append("<h3 class = 'full-name'>" + tweetData.user.name + "</h3> <span class = 'handle'>" + tweetData.user.handle + "</span>")
     $($header).append("<img src =" + tweetData.user.avatars.regular + ">")
-    $($tweetHTML).append($header)
-    $($tweetHTML).append($tweetText)
-    $($tweetHTML).append($footer)
+    $($tweetHTML).append($header, $tweetText, $footer)
     return $tweetHTML
   };
 
   function renderTweets(tweets) {
+    $("section.all-tweets").empty();
     tweets.forEach(function(eachTweet) {
       var $tweet = createTweetElement(eachTweet);
       $("section.all-tweets").append($tweet);
     });
   };
 
-  renderTweets(data);
+  loadTweets();
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -10,4 +10,15 @@ $(function() {
       $(".counter").eq(0).css( "color", "black")
     }
   })
+  $("#tweet-submit").on("submit", function(){
+    const $ogNum = $(".new-tweet .counter").contents();
+    let countNum  = 140 - $(this).val().length;
+    $ogNum.replaceWith(countNum);
+    if (countNum < 0) {
+      $(".counter").eq(0).css( "color", "red")
+    }
+    else {
+      $(".counter").eq(0).css( "color", "black")
+    }
+  })
 });
